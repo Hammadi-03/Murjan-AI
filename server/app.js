0a2e2d78a09ae74b55a0d06e2e788c5b03cbf61e
Hammadi-03 <[200~abdulazizhammadi809@gmail.com>
2026-03-24 00:29:21 +0700

    innirt

--- server/app.js
+++ server/app.js
@@ -10,6 +10,9 @@ const app = express();
 
 const ALLOWED_ORIGINS = [
   'http://localhost:5173',
+  'http://localhost:5174',
+  'http://localhost:5175',
+  'http://localhost:5176',
   'http://localhost:4173',
   'https://murjan.my.id',
   process.env.FRONTEND_ORIGIN,
