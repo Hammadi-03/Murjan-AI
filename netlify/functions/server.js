4066d9705c3ddb32b07fb6f5cabdd2316f54d6ff
Hammadi-03 <[200~abdulazizhammadi809@gmail.com>
2026-03-24 09:12:19 +0700

    innit

--- netlify/functions/server.js
+++
@@ -1,5 +0,0 @@
-import serverless from 'serverless-http';
-import app from '../../server/app.js';
-
-// Wraps the Express application into a handler format that Netlify AWS Lambdas can understand.
-export const handler = serverless(app, { basePath: '/.netlify/functions/server' });
